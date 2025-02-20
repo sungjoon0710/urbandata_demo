f37ba76b2a538b9a7b31d2422d44f81cbcbde5fe
math-eaton <mjh2241@columbia.edu>
2025-02-20 02:24:34 -0500

    test commit
    Demo for committing a new file

---
+++ new.py
@@ -0,0 +1,8 @@
+# I am creating a new file to test the git commit and push process
+# I am adding some new content to this file
+# I am adding some more content to this file
+# I am adding even more content to this file
+# I am adding some more content to this file
+# I am adding some more content to this file
+# I am adding some more content to this file
+# I cant believe copilot is a type of italian food
